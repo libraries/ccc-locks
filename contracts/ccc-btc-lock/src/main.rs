25f493b1e8909fa68eda14fe826414b448d59374
xjd <xjd@cryptape.com>
2024-07-16 09:02:48 +0800

    Refine main.rs

--- contracts/ccc-btc-lock/src/main.rs
+++ contracts/ccc-btc-lock/src/main.rs
@@ -1,17 +1,11 @@
 #![no_std]
-#![cfg_attr(not(test), no_main)]
-
-#[cfg(test)]
-extern crate alloc;
+#![no_main]
 
 mod entry;
 mod error;
 
-#[cfg(not(test))]
 use ckb_std::default_alloc;
-#[cfg(not(test))]
 ckb_std::entry!(program_entry);
-#[cfg(not(test))]
 default_alloc!(4 * 1024, 1400 * 1024, 64);
 
 use entry::entry;
@@ -19,6 +13,10 @@ use entry::entry;
 pub fn program_entry() -> i8 {
     match entry() {
         Ok(_) => 0,
-        Err(e) => e as i8,
+        Err(e) => {
+            let result = e as i8;
+            assert!(result != 0);
+            result
+        }
     }
 }
